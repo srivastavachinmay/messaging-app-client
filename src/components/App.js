5213331a6d3004b845342c581c036f95ac4c3852
Chinmay Srivastava <61424879+srivastavachinmay@users.noreply.github.com>
2021-11-24 13:35:29 +0530

    useLocalStorage Custom Hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import Login from "./Login";
+import Login             from "./Login";
+import useLocalStorage   from "./hooks/useLocalStorage";
 
 function App() {
-  return (
-    <Login/>
-  );
+    const [id, setId] = useLocalStorage('id')
+    return (
+        <>
+            {id}
+            <Login onIdSubmit={setId}/>
+        </>
+    );
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,31 @@
+import React, {useRef}           from 'react'
+import {Container, Form, Button} from 'react-bootstrap'
+import {v4 as uuidV4 }          from 'uuid';
 
-import React, { useRef } from 'react'
-import { Container, Form,Button } from 'react-bootstrap'
-function Login() {
+function Login({onIdSubmit}) {
     const idRef = useRef();
 
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(idRef.current)
+        onIdSubmit(idRef.current)
+    }
+
+    function createNewId(){
+        onIdSubmit(uuidV4())
+    }
 
     return (
-        <Container className="align-items-center d-flex" style={{ height: '100vh' }}>
-            <Form className='w-100'>
-                <Form.Group>
+        <Container className="align-items-center d-flex" style={{height: '100vh'}}>
+            <Form onSubmit={handleSubmit} className='w-100'>
+                <Form.Group className={'mb-2'}>
                     <Form.Label>
                         Enter Your ID
                     </Form.Label>
-                    <Form.Control type={"text"} ref={idRef} required></Form.Control>
+                    <Form.Control type={"text"} ref={idRef} required/>
                 </Form.Group>
-                <Button type='submit' className='mr-2'>Login</Button>
-                <Button variant='secondary'>Create a new Id</Button>
+                <Button type='submit' className="me-2">Login</Button>
+                <Button onClick={createNewId}  variant='secondary'>Create a new Id</Button>
             </Form>
         </Container>
     )
